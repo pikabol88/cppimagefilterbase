bb30618a871388840c0e0c675067b8f70517e404
pikabol88 <nena.2000@mail.ru>
2020-10-12 13:57:21 +0300

    Update filter_manager.cpp

--- filter_manager.cpp
+++ filter_manager.cpp
@@ -13,8 +13,9 @@ void filter_manager::calculateArea(rect_t rect, image_data &image) {
     applicationArea.up = (rect.up) ? image.h / rect.up : 0;
     applicationArea.right = (rect.right) ? image.w / rect.right : 0;
     applicationArea.bottom = (rect.bottom) ? image.h / rect.bottom : 0;
-    applicationArea.left = (rect.left) ? image.w / rect.left : 0;
+    applicationArea.left = (rect.left) ? image.w / rect.left : 0;  
 }
+
 bool search_substring(std::string str, std::string substring) {
     std::transform(str.begin(), str.end(), str.begin(), ::tolower);
     size_t pos = str.find(substring);
@@ -31,7 +32,7 @@ void filter_manager::decideFilterType(std::string name) {
         type = BLACK_WHITE_FILTER;
     }
     else if (search_substring(name, "threshold")) {
-        //type = THRESHOLD_FILTER;
+        type = THRESHOLD_FILTER;
        
     }
     else if (search_substring(name, "edge")) {
@@ -56,10 +57,12 @@ filter * filter_manager::getFilter() {
         filter = new threshold_filter(applicationArea);
         return filter;
     case BLUR_FILTER:
-        filter = new blur_filter(applicationArea);
+        filter = new red_filter(applicationArea);
+       // filter = new blur_filter(applicationArea);
         return filter;
     case EDGE_FILTER:
-        filter = new edge_filter(applicationArea);
+        filter = new red_filter(applicationArea);
+       // filter = new edge_filter(applicationArea);
         return filter;
     default:
         break;
