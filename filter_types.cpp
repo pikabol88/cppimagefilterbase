274026d97ec8ad947af863b50666d48435c655ea
pikabol88 <nena.2000@mail.ru>
2020-10-20 12:10:43 +0300

    Update filter_types.cpp

--- filter_types.cpp
+++ filter_types.cpp
@@ -23,10 +23,7 @@ void black_white_filter::apply_filter(image_data & image) {
         for (int j = filterArea.left;j < filterArea.right;j++) {
             int index = getPixelIndex(image, i, j);
             int intensity = findIntensity(image, index);
-            image.pixels[index] = intensity;
-            image.pixels[index + 1] = intensity;
-            image.pixels[index + 2] = intensity;
-
+            image.pixels[index] = image.pixels[index + 1] = image.pixels[index + 2] =intensity;
         }
     }
 }
@@ -43,42 +40,34 @@ threshold_filter::~threshold_filter() {
 
 void threshold_filter::apply_filter(image_data & image) {
     bw_filter->apply_filter(image);
-  //  image_data imageCopy = image.copy();
-    std::vector<stbi_uc>pixels;
+    std::vector<int>pixelsToZero;
     for (int i = filterArea.up; i < filterArea.bottom;i++) {
         for (int j = filterArea.left;j < filterArea.right;j++) {
             int index = getPixelIndex(image, i, j);           
-            int median = findMedian(image, i, j);           
-          //  int median = findMedian(imageCopy, i, j);
+            int median = findMedian(image, i, j);          
             if (findIntensity(image, index) < median) {
-                pixels.push_back((image.pixels[index]));
-                /*image.pixels[index] = 0;
-                image.pixels[index + 1] = 0;
-                image.pixels[index + 2] = 0;*/
+                pixelsToZero.push_back(index);
             }
         }
     }
-    int length = pixels.size();
-    for (int i = 0;i < length;i+=3) {
-        pixels[i] = (pixels[i+1]) = (pixels[i+2]) = 0;
+    for (int index: pixelsToZero) {
+        image.pixels[index] = image.pixels[index + 1] = image.pixels[index + 2] = 0;
     }
-    pixels.clear();
-    //imageCopy.deleteCopy();
 }
 
 int threshold_filter::findMedian(image_data &image, int i, int j) {
     std::vector<stbi_uc>pixelsIntensity;
     int x = i - 2;
     int y = j - 2;
-    for (int ii = 0;ii < 5;ii++) {
-        for (int jj = 0;jj < 5;jj++) {
+    for (int ii = 0; ii < 5; ii++) {
+        for (int jj = 0; jj < 5; jj++) {
             if (isPixelExist(image, x + ii, y + jj)) {
                 int index = getPixelIndex(image, x + ii, y + jj);
                 pixelsIntensity.push_back(findIntensity(image, index));
             }
         }
     }
-    std::sort(pixelsIntensity.begin(), pixelsIntensity.end());
+    nth_element(pixelsIntensity.begin(), pixelsIntensity.begin() + pixelsIntensity.size() / 2, pixelsIntensity.end());
     int size = pixelsIntensity.size();
     if (size % 2 == 1) {
         return pixelsIntensity[size / 2];
@@ -87,6 +76,8 @@ int threshold_filter::findMedian(image_data &image, int i, int j) {
         return int(0.5*(pixelsIntensity[size / 2 - 1] + pixelsIntensity[size / 2]));
     }
 }
+
+
 
 
 edge_filter::edge_filter(rect_t & area) :convolution_filter(area) {
